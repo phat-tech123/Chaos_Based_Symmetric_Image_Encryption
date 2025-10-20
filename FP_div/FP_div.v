cef5b99b5186e9bf3905c3052c4a055d918181c4
phat-tech123 <caovinhphat1904@gmail.com>
2025-10-20 12:40:12 +0700

    done all arthrmetic hardware

--- FP_div/FP_div.v
+++ FP_div/FP_div.v
@@ -39,12 +39,12 @@ end
 //---------------------------- EXTRACT ----------------------------//
 
 // Stage 1: Add exponents and determine sign
-wire [FRACTION:0] mant_a = {1'b1, frac_a};
-wire [FRACTION:0] mant_b = {1'b1, frac_b};
+wire [FRACTION+3:0] mant_a = {1'b1, frac_a, 3'b000}; 	//including the guard bit, the round bit, the sticky bit
+wire [FRACTION+3:0] mant_b = {1'b1, frac_b, 3'b000}; 	//including the guard bit, the round bit, the sticky bit
 wire sign_res = sign_a ^ sign_b;
 wire signed [EXPONENT:0] expo_diff = ($signed({1'b0, expo_a}) - $signed(BIAS)) - ($signed({1'b0, expo_b}) - $signed(BIAS));
 
-reg [1 + (EXPONENT+1) + (FRACTION+1)*2 - 1 : 0] minus_expo_deter_sign;
+reg [1 + (EXPONENT+1) + (FRACTION+3+1)*2 - 1 : 0] minus_expo_deter_sign;
 
 always@(posedge clk or negedge reset_n) begin
 	if(!reset_n)
@@ -56,26 +56,26 @@ end
 // Stage 2: Divide significands
 wire sign_res_s1;
 wire signed [EXPONENT:0] expo_diff_s1;
-wire [FRACTION:0] mant_a_s1; 	//dividend
-wire [FRACTION:0] mant_b_s1; 	//divisor
+wire [FRACTION+3:0] mant_a_s1; 	//dividend
+wire [FRACTION+3:0] mant_b_s1; 	//divisor
 assign {sign_res_s1, expo_diff_s1, mant_a_s1, mant_b_s1} = minus_expo_deter_sign;
 
-reg [2*(FRACTION+1)-1:0] divisor;
-reg [2*(FRACTION+1)-1:0] remainder;
-reg [FRACTION:0] quotient;
+reg [2*(FRACTION+3+1)-1:0] divisor;
+reg [2*(FRACTION+3+1)-1:0] remainder;
+reg [FRACTION+3:0] quotient;
 
 
-reg [1 + (EXPONENT+1) + (FRACTION+1) - 1 : 0] div_significands;
+reg [1 + (EXPONENT+1) + (FRACTION+3+1) - 1 : 0] div_significands;
 integer i;
 always@(posedge clk or negedge reset_n) begin
 	if(!reset_n)
 		div_significands <= 0;
 	else begin
-		divisor   = {{(FRACTION+1){1'b0}}, mant_b_s1};
-		remainder = {{(FRACTION+1){1'b0}}, mant_a_s1};
+		divisor   = {{(FRACTION+3+1){1'b0}}, mant_b_s1};
+		remainder = {{(FRACTION+3+1){1'b0}}, mant_a_s1};
 		quotient  = 0;
 
-		for(i = 0; i < FRACTION + 1; i = i+1) begin
+		for(i = 0; i < FRACTION + 3 + 1; i = i+1) begin
 			if(remainder >= divisor) begin
 				remainder = remainder - divisor;
 				quotient = (quotient << 1) | 1'b1;
@@ -92,14 +92,14 @@ end
 // Stage 3: normalize
 wire sign_res_s2;
 wire signed [EXPONENT:0] expo_diff_s2;
-wire [FRACTION:0] mant_res_s2;
+wire [FRACTION+3:0] mant_res_s2;
 
 assign {sign_res_s2, expo_diff_s2, mant_res_s2} = div_significands;
 
 reg signed [EXPONENT:0] expo_diff_s2_r;
-reg [FRACTION:0] mant_res_s2_r;
+reg [FRACTION+3:0] mant_res_s2_r;
 
-reg [1 + (EXPONENT+1) + (FRACTION+1) - 1 : 0] normalize;
+reg [1 + (EXPONENT+1) + (FRACTION+3+1) - 1 : 0] normalize;
 //integer j;
 always @(posedge clk or negedge reset_n) begin
     	if (!reset_n) begin
@@ -108,8 +108,8 @@ always @(posedge clk or negedge reset_n) begin
             	mant_res_s2_r 	 = mant_res_s2;
             	expo_diff_s2_r = expo_diff_s2;
         
-            	for(i = 0; i < FRACTION+1; i = i+1) begin 	//UNDERFLOW
-                	if(mant_res_s2_r[FRACTION] == 0 && mant_res_s2_r != 0) begin
+            	for(i = 0; i < FRACTION+3+1; i = i+1) begin 	//UNDERFLOW
+                	if(mant_res_s2_r[FRACTION+3] == 0 && mant_res_s2_r != 0) begin
                     		mant_res_s2_r = mant_res_s2_r << 1;
                     		expo_diff_s2_r = expo_diff_s2_r - 1;
                 	end
@@ -122,12 +122,12 @@ end
 // Stage 4: rounding 
 wire sign_res_s3;
 wire signed [EXPONENT:0] expo_diff_s3;
-wire [FRACTION:0] mant_res_s3;
+wire [FRACTION+3:0] mant_res_s3;
 
 assign {sign_res_s3, expo_diff_s3, mant_res_s3} = normalize;
 
 wire [FRACTION-1:0] mant_rounded;
-assign mant_rounded = mant_res_s3[FRACTION-1:0];
+assign mant_rounded = mant_res_s3[FRACTION+3-1:3];
 
 wire [EXPONENT-1:0] expo_rounded;
 assign expo_rounded = expo_diff_s3[EXPONENT-1:0] + BIAS;
