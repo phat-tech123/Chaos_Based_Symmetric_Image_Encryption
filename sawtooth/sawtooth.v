35fc961c004d00496d2c98ad89b6452d5c99c836
phat-tech123 <caovinhphat1904@gmail.com>
2025-10-22 17:09:36 +0700

    got true result but not yet optimize

--- sawtooth/sawtooth.v
+++ sawtooth/sawtooth.v
@@ -1,5 +1,5 @@
 module sawtooth#(
-	parameter PRECISION = 32,
+	parameter PRECISION = 32
 )(
 	input clk,
 	input reset_n,
@@ -8,16 +8,233 @@ module sawtooth#(
 	output reg [PRECISION-1:0] result
 );
 
-wire [PRECISION-1:0] div1_out;
-FP_div #(.PRECISION(32), .EXPONENT(8), .FRACTION(23), .BIAS(127)) div1(
-	.clk(clk),
-	.reset_n(reset_n),
-	.a_operand(x), 
-	.b_operand(epsilon), 
-	.result(div1_out)
+parameter S1 	= 0;
+parameter S2 	= 1;
+parameter S3 	= 2;
+parameter S4 	= 3;
+parameter S5 	= 4;
+parameter S6 	= 5;
+parameter S7 	= 6;
+parameter S8 	= 7;
+parameter STOP 	= 8;
+
+reg [2:0] counter;
+reg [3:0] state;
+
+reg [PRECISION-1:0] L;
+reg [PRECISION-1:0] div_a_op, div_b_op, mul_a_op, mul_b_op, add_a_op, add_b_op;
+wire [PRECISION-1:0] div_out, mul_out, add_out;
+
+FP_div #(.PRECISION(32), .EXPONENT(8), .FRACTION(23), .BIAS(127)) div(
+	.clk(clk), .reset_n(reset_n),
+	.a_operand(div_a_op), .b_operand(div_b_op), 
+	.result(div_out)
 );
 
+FP_mul #(.PRECISION(32), .EXPONENT(8), .FRACTION(23), .BIAS(127)) mul(
+	.clk(clk), .reset_n(reset_n),
+	.a_operand(mul_a_op), .b_operand(mul_b_op), 
+	.result(mul_out)
+);
 
-wire [PRECISION-1:0] L;
+FP_adder #(.PRECISION(32), .EXPONENT(8), .FRACTION(23), .BIAS(127)) add(
+	.clk(clk), .reset_n(reset_n),
+	.a_operand(add_a_op),  .b_operand(add_b_op), 
+	.result(add_out)
+);
+
+always@(posedge clk or negedge reset_n) begin 
+	if(!reset_n) begin
+		state 	<= S1;
+		counter <= 0;
+	end else if (counter == 3'd4) begin
+		counter <= 0;
+		case(state)
+			S1: state <= S2;
+			S2: state <= S3;
+			S3: state <= S4;
+			S4: state <= S5;
+			S5: state <= S6;
+			S6: state <= S7;
+			S7: state <= S8;
+			S8: state <= STOP;
+			default: state <= state;
+		endcase
+	end else begin
+		counter <= counter + 1;
+	end
+end
+
+always@(posedge clk or negedge reset_n) begin
+	if(!reset_n) begin
+		L 		<= 0;
+		div_a_op 	<= 0;
+		div_b_op 	<= 0;
+		mul_a_op 	<= 0;
+		mul_b_op 	<= 0;
+		add_a_op 	<= 0;
+		add_b_op 	<= 0;
+		result      <= 0;
+	end else begin
+		case(state)
+			// Stage 1 : x/epsilon
+			S1: begin
+				div_a_op 	<= x;
+				div_b_op 	<= epsilon;
+				mul_a_op 	<= mul_a_op;
+				mul_b_op 	<= mul_b_op;
+				add_a_op 	<= add_a_op;
+				add_b_op 	<= add_b_op;
+			end
+
+			// Stage 2 : x/epsilon + 1.0
+			S2: begin
+				div_a_op 	<= div_a_op;
+				div_b_op 	<= div_b_op;
+				mul_a_op 	<= mul_a_op;
+				mul_b_op 	<= mul_b_op;
+				add_a_op 	<= 32'h3f800000; //1.0
+				add_b_op 	<= div_out;
+			end
+
+			// Stage 3 : (x/epsilon + 1.0) / 2.0 = (x/epsilon + 1.0) * 0.5 
+			S3: begin
+				div_a_op 	<= div_a_op;
+				div_b_op 	<= div_b_op;
+				mul_a_op 	<= add_out; 
+				mul_b_op 	<= 32'h3f000000; //0.5
+				add_a_op 	<= add_a_op;
+				add_b_op 	<= add_b_op;
+			end
+
+			// Stage 4 : L = floor((x/epsilon + 1.0) * 0.5) 
+			S4: begin 
+				L 		<= floor_ieee32(mul_out);
+				div_a_op 	<= div_a_op;
+				div_b_op 	<= div_b_op;
+				mul_a_op 	<= mul_a_op;
+				mul_b_op 	<= mul_b_op;
+				add_a_op 	<= add_a_op;
+				add_b_op 	<= add_b_op;
+			end
+
+			// Stage 5 : -2*L
+			S5: begin
+				div_a_op 	<= div_a_op;
+				div_b_op 	<= div_b_op;
+				mul_a_op 	<= 32'hc0000000; //-2.0
+				mul_b_op 	<= L;
+				add_a_op 	<= add_a_op;
+				add_b_op 	<= add_b_op;
+			end
+
+			// Stage 6 : -2*L*epsilon
+			S6: begin
+				div_a_op 	<= div_a_op;
+				div_b_op 	<= div_b_op;
+				mul_a_op 	<= mul_out;
+				mul_b_op 	<= epsilon;
+				add_a_op 	<= add_a_op;
+				add_b_op 	<= add_b_op;
+			end
+
+			// Stage 7 : x + (-2*L*epsilon)
+			S7: begin
+				div_a_op 	<= div_a_op;
+				div_b_op 	<= div_b_op;
+				mul_a_op 	<= mul_a_op;
+				mul_b_op 	<= mul_b_op;
+				add_a_op 	<= x;
+				add_b_op 	<= mul_out;
+			end
+
+			// Stage 8 : pow(-1.0, l) * (x - 2 * l * epsilon)
+			S8: begin
+				div_a_op 	<= div_a_op;
+				div_b_op 	<= div_b_op;
+				mul_a_op 	<= (is_odd_ieee32(L)) ? 32'hbf800000 : 32'h3f800000;
+				mul_b_op 	<= add_out;
+				add_a_op 	<= add_a_op;
+				add_b_op 	<= add_b_op;
+			end
+			
+			STOP: begin
+				div_a_op 	<= div_a_op;
+				div_b_op 	<= div_b_op;
+				mul_a_op 	<= mul_a_op;
+				mul_b_op 	<= mul_a_op;
+				add_a_op 	<= add_a_op;
+				add_b_op 	<= add_b_op;
+				result <= mul_out;
+			end
+		endcase
+
+	end
+end
+
+function signed [31:0] floor_ieee32(input [31:0] x);
+	reg sign;
+	reg [7:0] exponent;
+	reg [22:0] fraction;
+	reg [31:0] result;
+	integer E, frac_bits;
+	reg [23:0] mantissa;
+	reg [23:0] mask;
+
+	begin
+		sign     = x[31];
+		exponent = x[30:23];
+		fraction = x[22:0];
+
+		if (exponent < 127) begin
+			if (sign)
+				result = 32'hBF800000; // -1.0
+			else
+				result = 32'h00000000; // 0.0
+		end
+		else if (exponent >= 150) begin
+			result = x;
+		end
+		else begin
+			E = exponent - 127;
+			mantissa = {1'b1, fraction}; // 1.f
+			frac_bits = 23 - E;
+
+			mask = ~((1 << frac_bits) - 1);
+			mantissa = mantissa & mask;
+
+			if (sign && ((x[22:0] & ~mask) != 0))
+				mantissa = mantissa + (1 << frac_bits);
+
+			result = {sign, exponent, mantissa[22:0]};
+		end
+
+		floor_ieee32 = result;
+	end
+endfunction
+
+function is_odd_ieee32(input [31:0] x);
+	reg [7:0] exponent;
+    	reg [22:0] fraction;
+    	reg [23:0] mantissa;
+    	integer int_bits;
+    	reg [23:0] shifted;
+
+	begin
+		exponent = x[30:23];
+		fraction = x[22:0];
+		mantissa = {1'b1, fraction};       
+		int_bits = exponent - 127;
+
+		if (exponent < 127)
+			is_odd_ieee32 = 1'b0;    
+		else if (int_bits >= 24)
+			is_odd_ieee32 = 1'b0;  
+		else begin
+			shifted = mantissa >> (23 - int_bits); 
+			is_odd_ieee32 = shifted[0];           
+		end
+	end
+endfunction
 
 endmodule
---
+++ sawtooth/sawtooth_tb.v
@@ -0,0 +1,87 @@
+`timescale 1ns/1ps
+
+module FP_sawtooth_tb;
+
+// =========================================
+// Parameters
+// =========================================
+parameter PRECISION = 32;
+
+// =========================================
+// DUT I/O
+// =========================================
+reg  clk;
+reg  reset_n;
+reg  [PRECISION-1:0] a_operand;
+reg  [PRECISION-1:0] b_operand;
+wire [PRECISION-1:0] result;
+
+// =========================================
+// Instantiate DUT
+// =========================================
+sawtooth #(
+    .PRECISION(PRECISION)
+) uut (
+    .clk(clk),
+    .reset_n(reset_n),
+    .x(a_operand),
+    .epsilon(b_operand),
+    .result(result)
+);
+
+// =========================================
+// Clock generation: 100MHz
+// =========================================
+initial begin
+    clk = 0;
+    forever #5 clk = ~clk;
+end
+
+// =========================================
+// Stimulus
+// =========================================
+initial begin
+    // Init
+    reset_n   = 0;
+    a_operand = 0;
+    b_operand = 0;
+
+    // Wait some cycles before releasing reset
+    #20;
+    reset_n = 1;
+
+    // Wait a few clocks
+    #10;
+
+    // Apply test values
+    // a_operand = 1.75
+    // b_operand = 0.05
+    //a_operand = 32'h3fe00000; 
+    a_operand = 32'h3f8fc265; 
+    b_operand = 32'h3d4ccccd;
+
+    // Wait enough time for pipeline result
+    #2000;
+
+    $finish;
+end
+
+// =========================================
+// Dump waveform
+// =========================================
+initial begin
+    $dumpfile("sawtooth.vcd");
+    $dumpvars(0, FP_sawtooth_tb);
+end
+
+// =========================================
+// Optional monitor
+// =========================================
+initial begin
+ //   $display("Time\t\tdiv_out\tmul_out\tadd_out\tresult");
+    $display("Time\t state \tdiv_out       \tmul_out   \tadd_out      \tresult");
+    $monitor("%0t\t%d\t%h\t%h\t%h\t%h", $time, uut.state,uut.div_out, uut.mul_out, uut.add_out, result);
+end
+
+endmodule
+
